e482aea59f4bf2b6a2936aed1ae5594a1c2d508f
Julien <julien.salvat2003@gmail.com>
2023-12-13 19:06:03 +0100

    Modification du viesuel d'ajout de nouveau produit

--- Templates/Produit/Ajout.twig.php
+++ Templates/Produit/Ajout.twig.php
@@ -6,33 +6,42 @@
 
 {% block content %}
 {% if session.admin is defined %}
-    <h1 class="fw-light">Ajouter un Produit</h1>
-    <p class="lead text-muted">Formulaire pour ajouter un produit.</p>
+    <div class="container">
+        <h1 class="fw-light mt-5">Ajouter un Produit</h1>
+        <p class="lead text-muted">Formulaire d'ajout de nouveau produit.</p>
 
-    <form method="post" action="/Produit/ajouterProduit">
-        <label for="cat_name">Catégorie:</label>
-        <input type="text" id="cat_name" name="cat_name" required>
-        <br>
+        <form method="post" action="/Produit/ajouterProduit" class="mt-4">
+            <div class="mb-2">
+                <label for="cat_name" class="form-label">Catégorie:</label>
+                <input type="text" id="cat_name" name="cat_name" class="form-control" required>
+            </div>
 
-        <label for="name">Nom:</label>
-        <input type="text" id="name" name="name" required>
-        <br>
+            <div class="mb-2">
+                <label for="name" class="form-label">Nom:</label>
+                <input type="text" id="name" name="name" class="form-control" required>
+            </div>
 
-        <label for="description">Description:</label>
-        <input type="text" id="description" name="description" required></input>
-        <br>
+            <div class="mb-2">
+                <label for="description" class="form-label">Description:</label>
+                <textarea id="description" name="description" class="form-control" required></textarea>
+            </div>
 
-        <label for="image">Image:</label>
-        <input type="text" id="image" name="image" required>
-        <br>
+            <div class="mb-2">
+                <label for="image" class="form-label">Image URL:</label>
+                <input type="text" id="image" name="image" class="form-control" required>
+            </div>
 
-        <label for="price">Prix:</label>
-        <input type="text" id="price" name="price" required>
-        <br>
+            <div class="mb-2">
+                <label for="price" class="form-label">Prix:</label>
+                <input type="text" id="price" name="price" class="form-control" required>
+            </div>
 
-        <input type="submit" value="Ajouter">
-    </form>
-    {% else %}
-        Vous n'avez pas l'autorisation d'acceder a cette page
-    {% endif %}
+            <button type="submit" class="btn btn-primary">Ajouter</button>
+        </form>
+    </div>
+{% else %}
+    <div class="container mt-5">
+        <p class="lead text-danger">Vous n'avez pas l'autorisation d'accéder à cette page.</p>
+    </div>
+{% endif %}
 {% endblock %}
