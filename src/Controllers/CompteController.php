2ccca7c8a29318dbb5ce6708acf94d8f16e72d3d
Julien <julien.salvat2003@gmail.com>
2023-12-14 10:35:48 +0100

    Ajout du système de compte

--- src/Controllers/CompteController.php
+++ src/Controllers/CompteController.php
@@ -1,19 +1,55 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\Compte;
 
 class CompteController
 {
     public function index()
     {
+        $model = new Compte;
+        $compte = $model->getCustomer();
+        if ($compte == false){
+            $model->createEmptyCustomer();
+            $compte = $model->getCustomer();
+        }
         $twig = new Twig;
-        
-        $twig->afficherpage('Compte');
+        $twig->afficherpage('Compte','index',['compte' => $compte]);
     }
 
     public function ModifierCompte()
-    {
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Récupérer les données du formulaire POST
+        $forname = $_POST['firstname'] ?? '';
+        $surname = $_POST['lastname'] ?? '';
+        $add1 = $_POST['add1'] ?? '';
+        $add2 = $_POST['add2'] ?? '';
+        $city = $_POST['city'] ?? '';
+        $postcode = $_POST['postcode'] ?? '';
+        $phone = $_POST['phone'] ?? '';
+        $email = $_POST['email'] ?? '';
 
+        // Vérifier si les valeurs sont définies
+        if (!empty($forname) && !empty($surname) && !empty($add1) && !empty($city) && !empty($postcode) && !empty($phone) && !empty($email)) {
+            // Obtenez l'ID de la session
+            $customer_id = $_SESSION['id'];
+
+            // Instancier le modèle Compte
+            $model = new Compte;
+
+            // Appeler la fonction modifieCustomer du modèle
+            $model->modifieCustomer($customer_id, $forname, $surname, $add1, $add2, $city, $postcode, $phone, $email, 1);
+
+            // Rediriger vers une autre page après la modification
+            header('Location: /Compte');
+            exit();
+        }
+        // Cas où on utilise pas la méthode POST
+        header('Location: /');
+        exit();
     }
+}
+
     
 }
--- src/Models/Compte.php
+++ src/Models/Compte.php
@@ -5,10 +5,10 @@ use PDO;
 
 class Compte extends Model
 {
-    public function getDeliveryAddresses()
+    public function getcustomers()
     {
         // Requête SQL pour récupérer les données de la table delivery_addresses
-        $sql = "SELECT * FROM delivery_addresses WHERE ";
+        $sql = "SELECT * FROM customers WHERE ";
         
         // Exécution de la requête
         $resultat = $this->executerRequete($sql);
@@ -19,4 +19,53 @@ class Compte extends Model
         // Retourner les données
         return $donnees;
     }
+
+    public function getCustomer()
+    {
+        //  On récupère l'id du customer qui est stockée dans la variable de session id
+        $customerId = $_SESSION['id'];
+
+        // Requête SQL pour récupérer un client par son ID
+        $sql = "SELECT * FROM customers WHERE id = ?";
+
+        // Exécution de la requête avec l'ID en paramètre
+        $resultat = $this->executerRequete($sql, [$customerId]);
+
+        // Récupération des données
+        $customer = $resultat->fetch(PDO::FETCH_ASSOC);
+
+        return $customer;
+    }
+
+    public function modifieCustomer($customer_id, $forname, $surname, $add1, $add2, $add3, $postcode, $phone, $email, $registered)
+    {
+        // Requête SQL pour mettre à jour un client existant
+        $sql = "UPDATE customers 
+                SET forname = ?, surname = ?, add1 = ?, add2 = ?, add3 = ?, postcode = ?, phone = ?, email = ?, registered = ?
+                WHERE id = ?";
+
+        // Paramètres de la requête
+        $params = [$forname, $surname, $add1, $add2, $add3, $postcode, $phone, $email, $registered, $customer_id];
+
+        // Exécution de la requête
+        $this->executerRequete($sql, $params);
+    }
+
+
+    public function createEmptyCustomer()
+    {
+        // ID récupéré à partir de la session
+        $id = $_SESSION['id'];
+
+        // Requête SQL pour insérer un nouveau client avec des valeurs par défaut ou vides
+        $sql = "INSERT INTO customers (id, forname, surname, add1, add2, add3, postcode, phone, email, registered) 
+                VALUES (?, '', '', '', '', '', '', '', '', 0)";
+
+        // Paramètres de la requête
+        $params = [$id];
+
+        // Exécution de la requête
+        $this->executerRequete($sql, $params);
+    }
+
 }
